1a4b07302f1def6be58301e84224cef68a07a1be
kihifung <kihifung@gmail.com>
2021-09-13 01:07:11 +0800

    finish practice
    ch13-14

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,48 @@
+class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:edit, :update, :vote, :destroy]
+
+  def index
+    @candidate = Candidate.all
+  end
+  def new
+    @candidate = Candidate.new
+  end  
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save    # 成功
+      redirect_to candidates_path, notice: "新增候選人成功!"
+    else  # 失敗
+      render :new
+    end
+  end
+  def edit
+  end
+
+  def update
+    if @candidate.update(candidate_params)  # 成功
+      redirect_to candidates_path, notice: "資料更新成功!"
+    else  # 失敗
+      render :edit
+    end
+  end
+  def vote
+    @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
+    redirect_to candidates_path, notice: "完成投票!"
+    # redirect_to candidate_result_path, notice: "完成投票!"
+  end
+
+  def destroy
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: "候選人資料已刪除!"
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
+  end  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :candidates do 
+    member do
+      post :vote
+      get :result, to: "candidates#result"
+      post :show, to: "candidates#show"
+
+    end
+  end  
 end
